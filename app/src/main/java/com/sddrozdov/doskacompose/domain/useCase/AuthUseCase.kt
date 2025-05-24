03c5a3a23525c55ad21b24c719a10b1d8f3223a3
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-24 12:07:12 +0300

    auth useCase Email and pass

---
+++ app/src/main/java/com/sddrozdov/doskacompose/domain/useCase/AuthUseCase.kt
@@ -0,0 +1,56 @@
+package com.sddrozdov.doskacompose.domain.useCase
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseUser
+import com.sddrozdov.doskacompose.data.repository.AuthRepositoryImpl
+import javax.inject.Inject
+import kotlin.Result
+
+class AuthUseCase @Inject constructor(private val repository: AuthRepositoryImpl) {
+
+    suspend fun signUp(email: String, password: String): Result<FirebaseUser> {
+        if (email.isEmpty() || password.isEmpty()) {
+            return Result.failure(IllegalArgumentException("Email or password is empty"))
+        }
+//        val deleteResult = repository.deleteCurrentUser()
+//        if (deleteResult.isFailure) {
+//            // Логируем, но не прерываем
+//            Log.w("AuthUseCase", "Delete current user failed: ${deleteResult.exceptionOrNull()}")
+//        }
+
+        val signUpResult = repository.signUp(email, password)
+
+        if (signUpResult.isFailure) {
+            val ex = signUpResult.exceptionOrNull()
+            if (ex is FirebaseAuthUserCollisionException) {
+                // Попытка связать email с Google аккаунтом
+                val linkResult = repository.linkEmailToGmail(email, password)
+                if (linkResult.isFailure) {
+                    return Result.failure(linkResult.exceptionOrNull()!!)
+                }
+                return Result.success(repository.firebaseAuth.currentUser!!)
+            }
+            return Result.failure(ex!!)
+        }
+
+        return signUpResult
+    }
+
+    suspend fun signIn(email: String, password: String): Result<FirebaseUser> {
+        if (email.isEmpty() || password.isEmpty()) {
+            return Result.failure(IllegalArgumentException("Email or password is empty"))
+        }
+
+//        val deleteResult = repository.deleteCurrentUser()
+//        if (deleteResult.isFailure) {
+//            Log.w("AuthUseCase", "Delete current user failed: ${deleteResult.exceptionOrNull()}")
+//        }
+
+        return repository.signIn(email, password)
+    }
+
+    suspend fun sendVerificationEmail(user: FirebaseUser): Result<Unit> {
+        return repository.sendEmailVerification(user)
+    }
+}
