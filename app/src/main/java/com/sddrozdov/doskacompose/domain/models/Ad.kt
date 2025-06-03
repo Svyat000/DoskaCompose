0ae235c64546670c79c84b49803f60e0db7f91db
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-06-03 17:31:22 +0300

    add ad model and repo

---
+++ app/src/main/java/com/sddrozdov/doskacompose/domain/models/Ad.kt
@@ -0,0 +1,29 @@
+package com.sddrozdov.doskacompose.domain.models
+
+import java.io.Serializable
+
+data class Ad(
+    val key: String? = null,
+    val uid: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val price: String? = null,
+    val country: String? = null,
+    val city: String? = null,
+    val category: String? = null,
+    val email: String? = null,
+    val tel: String? = null,
+    val index: String? = null,
+    val time: String = "0",
+
+    val mainImage: String = "empty",
+    val image2: String = "empty",
+    val image3: String = "empty",
+
+    var viewsCounter: String = "0",
+    var emailsCounter: String = "0",
+    var callsCounter: String = "0",
+    var favoriteCounter: String = "0",
+
+    var isFavorite: Boolean = false
+) : Serializable
