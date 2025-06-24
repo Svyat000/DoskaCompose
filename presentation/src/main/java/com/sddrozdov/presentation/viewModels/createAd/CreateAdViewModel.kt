e87542b7e5925830ea6fbe478a4d90c8053a755f
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-06-24 09:18:44 +0300

    ref createAdViewModel

--- presentation/src/main/java/com/sddrozdov/presentation/viewModels/createAd/CreateAdViewModel.kt
+++ presentation/src/main/java/com/sddrozdov/presentation/viewModels/createAd/CreateAdViewModel.kt
@@ -14,9 +14,6 @@ import com.sddrozdov.presentation.states.createAd.CreateAdStates
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,15 +26,10 @@ class CreateAdViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
-    private val _state =
-        MutableStateFlow(savedStateHandle.get<CreateAdStates>(CREATE_AD_STATE) ?: CreateAdStates())
-    val state: StateFlow<CreateAdStates> = _state
+    val state =
+        savedStateHandle.getStateFlow(key = CREATE_AD_STATE, initialValue = CreateAdStates())
 
     init {
-        viewModelScope.launch {
-            _state.collect { savedStateHandle[CREATE_AD_STATE] = it }
-        }
-        println("ViewModel initialized. Current state: ${_state.value}")
         loadCategories()
         loadCountries()
     }
@@ -48,7 +40,8 @@ class CreateAdViewModel @Inject constructor(
             is CreateAdEvents.IsAuthorized -> TODO()
 
             is CreateAdEvents.OnCategorySelected -> {
-                _state.value = _state.value.copy(selectedCategoryId = event.categoryId)
+                savedStateHandle[CREATE_AD_STATE] =
+                    state.value.copy(selectedCategoryId = event.categoryId)
             }
 
             CreateAdEvents.ShowCountrySelection -> {
@@ -68,23 +61,27 @@ class CreateAdViewModel @Inject constructor(
             }
 
             is CreateAdEvents.OnCountryAndCitySelected ->
-                _state.update { currentState ->
-                    currentState.copy(
-                        selectedCountry = event.country,
-                        selectedCity = event.city,
-                        countryAndCitySelected = true
-                    )
-                }
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+                    selectedCountry = event.country,
+                    selectedCity = event.city,
+                    countryAndCitySelected = true
+                )
 
-            is CreateAdEvents.OnTitleChanged -> _state.update { currentState ->
-                currentState.copy(title = event.newTitle)
+
+            is CreateAdEvents.OnTitleChanged -> {
+                savedStateHandle[CREATE_AD_STATE] =
+                    state.value.copy(title = event.newTitle)
             }
 
-            is CreateAdEvents.OnDescriptionChanged -> _state.update { currentState ->
-                currentState.copy(description = event.newDescription)
+            is CreateAdEvents.OnDescriptionChanged -> {
+                savedStateHandle[CREATE_AD_STATE] =
+                    state.value.copy(description = event.newDescription)
             }
 
-            is CreateAdEvents.OnEmailChanged -> TODO()
+            is CreateAdEvents.OnEmailChanged -> {
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(email = event.newEmail)
+
+            }
 
 
             is CreateAdEvents.ImagesSelected -> TODO()
@@ -108,22 +105,19 @@ class CreateAdViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val categories = loadCategoriesFromRaw()
-                _state.update { currentState ->
-                    currentState.copy(
-                        categories = categories,
-                        isLoading = false
-                    )
-                }
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+                    categories = categories,
+                    isLoading = false
+                )
             } catch (e: Exception) {
-                _state.update { currentState ->
-                    currentState.copy(
-                        error = "Failed to load categories",
-                        isLoading = false
-                    )
-                }
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+                    error = "Failed to load categories",
+                    isLoading = false
+                )
             }
         }
     }
+
 
     private fun loadCategoriesFromRaw(): List<Category> {
         val inputStream = context.resources.openRawResource(R.raw.categories)
@@ -136,22 +130,20 @@ class CreateAdViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val countries = loadCountriesFromRaw()
-                _state.update { currentState ->
-                    currentState.copy(
-                        countries = countries,
-                        isLoading = false
-                    )
-                }
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+                    countries = countries,
+                    isLoading = false
+                )
+
             } catch (e: Exception) {
-                _state.update { currentState ->
-                    currentState.copy(
-                        error = "Failed to load countries",
-                        isLoading = false
-                    )
-                }
+                savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+                    error = "Failed to load countries",
+                    isLoading = false
+                )
             }
         }
     }
+
 
     private fun loadCountriesFromRaw(): List<Country> {
         val inputStream = context.resources.openRawResource(R.raw.countries)
@@ -161,40 +153,32 @@ class CreateAdViewModel @Inject constructor(
     }
 
     private fun selectCountry(country: Country) {
-        _state.update { currentState ->
-            currentState.copy(
-                selectedCountry = country,
-                isCountrySelectionVisible = false,
-                isCitySelectionVisible = true,
-                selectedCity = null
-            )
-        }
+        savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+            selectedCountry = country,
+            isCountrySelectionVisible = false,
+            isCitySelectionVisible = true,
+            selectedCity = null
+        )
     }
 
     private fun selectCity(city: String) {
-        _state.update { currentState ->
-            currentState.copy(
-                selectedCity = city,
-                isCitySelectionVisible = false
-            )
-        }
+        savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+            selectedCity = city,
+            isCitySelectionVisible = false
+        )
     }
 
     private fun showCountrySelection() {
-        _state.update { currentState ->
-            currentState.copy(
-                isCountrySelectionVisible = true,
-                isCitySelectionVisible = false
-            )
-        }
+        savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+            isCountrySelectionVisible = true,
+            isCitySelectionVisible = false
+        )
     }
 
     private fun showCitySelection() {
-        _state.update { currentState ->
-            currentState.copy(
-                isCountrySelectionVisible = false,
-                isCitySelectionVisible = true
-            )
-        }
+        savedStateHandle[CREATE_AD_STATE] = state.value.copy(
+            isCountrySelectionVisible = false,
+            isCitySelectionVisible = true
+        )
     }
 }
